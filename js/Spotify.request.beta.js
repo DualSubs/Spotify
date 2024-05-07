d013a3b1b98c7b69b1229d7ae8b52914388e4ac9
Virgil Clyne <Virgil@nanocat.me>
2024-05-07 13:31:26 +0800

    test(request): UA
    Update Spotify.request.beta.js

--- js/Spotify.request.beta.js
+++ js/Spotify.request.beta.js
@@ -899,7 +899,7 @@ function setCache(cache, cacheSize = 100) {
 	return cache;
 }
 
-const $ = new ENV("🍿 DualSubs: 🎵 Spotify v1.4.0(2) request.beta");
+const $ = new ENV("🍿 DualSubs: 🎵 Spotify v1.4.1(1004) request.beta");
 
 // 构造回复数据
 let $response = undefined;
@@ -996,6 +996,10 @@ $.log(`⚠ FORMAT: ${FORMAT}`, "");
 					}					//break; // 不中断，继续处理URL
 				case "GET":
 					if (PATH.startsWith("/color-lyrics/v2/track/")) {
+						if ($request.headers["Spotify-App-Version"]) $request.headers["Spotify-App-Version"] = "8.8.96.395";
+						if ($request.headers["spotify-app-version"]) $request.headers["spotify-app-version"] = "8.8.96.395";
+						if ($request.headers["User-Agent"]) $request.headers["User-Agent"] = "Spotify/8.8.96 iOS/17.2.1 (iPhone14,3)";
+						if ($request.headers["user-agent"]) $request.headers["user-agent"] = "Spotify/8.8.96 iOS/17.2.1 (iPhone14,3)";
 						let trackId = PATHs?.[3];
 						$.log(`🚧 调试信息`, `trackId: ${trackId}`, "");
 						let _request = JSON.parse(JSON.stringify($request));
